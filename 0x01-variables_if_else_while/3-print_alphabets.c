405df229679954d37dba2de8317e0eefe8b1f0f1
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-07-01 11:32:58 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -14,9 +14,9 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	for (low = 'A'; low >= 'Z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
