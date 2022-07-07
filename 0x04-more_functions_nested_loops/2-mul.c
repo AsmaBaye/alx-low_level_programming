4d6f56e663865b6ff47c219dbb1c7c93f10ca676
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-07-07 07:11:25 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ *@a: character
+ *@b: character
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int z;
+
+	z = (a * b);
+	return (z);
+}
