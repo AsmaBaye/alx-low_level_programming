ebb7ca3ca209e7f6d4df8e3c1dcec3923489fda0
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-07-11 12:22:15 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,20 @@
 #include "main.h"
-/**
- * print_numbers - check the code
- * Return: Always 0.
- */
+#include <stdio.h>
 
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * void: No Input Parameter
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		_putchar(i);
+		putchar(i);
 	}
-
-	_putchar("\n");
+	putchar('\n');
 }
