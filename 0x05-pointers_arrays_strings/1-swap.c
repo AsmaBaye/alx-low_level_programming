5ac837cedfaf9ed150a724bbf0f340a53730a2fd
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-07-11 05:21:45 -0700

    update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,15 @@
 #include "main.h"
+
 /**
  *swap_int - check the code
  *@a: > input 1
  *@b: > input 2
  */
 void swap_int(int *a, int *b)
-{   int c,  c = *a, *a = *b, *b = c;
+{   int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
 }
