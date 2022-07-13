d9b043d561accb984f30f3839b5c11bcfb57e681
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-07-13 08:57:22 -0700

    updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,36 +1,30 @@
 #include "main.h"
 /**
- * *leet - encodes a string into 1337.
- *
- * @ptr: array
- *
- * Return: Capitalized array
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer dest.
  */
-char *leet(char *ptr)
+
+char *leet(char *s)
 {
-	int i;
-	char abc;
-	int len = 0;
+int count = 0, i;
+int lower_case[] = {97, 101, 111, 116, 108};
+int upper_case[] = {65, 69, 79, 84, 76};
+int numbers[] = {52, 51, 48, 55, 49};
 
-	while (ptr[len] != '\0')
-	{
-		len++;
-	}
 
-	for (i = 0; i < len; i++)
-	{
-		abc = 'a';
-		while (abc <= 'z')
-		{
-			if (ptr[i] == 'a' || ptr[i] == 'A')
-				ptr[i] = 4;
-			else if (ptr[i] == 'e' || ptr[i] == 'E')
-			{
-				ptr[i] = 3;
-			}
-			abc++;
-		}
-	}
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 5; i++)
+{
+if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
+{
+*(s + count) = numbers[i];
+break;
+}
+}
+count++;
+}
 
-	return (ptr);
+return (s);
 }
