de0eaa443d29f22e667eba0f69b42cc0dde9744e
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-08-16 06:03:43 -0700

    add C files for more on linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint -  this function prints all the elements
+ * of a listint_t list.
+ * @h: linked list pointer.
+ * Return: counter.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t counter;
+
+	if (h == NULL)
+		return (0);
+
+	for (counter = 0; h != NULL; counter++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (counter);
+}
