20f7344ff87380abfb61880417bde07e275ae4db
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-12-14 12:20:28 -0800

    update 100-Makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,47 +1,21 @@
 #!/usr/bin/python3
-"""Defines a function to determine the perimiter of an island"""
 
 
 def island_perimeter(grid):
-    """Determine the perimeter of a grid"""
 
-    height = len(grid)
-    if grid[0]:
-        width = len(grid[0])
+    wd = len(grid[0])
+    ht = len(grid)
+    per = 0
 
-    perimeter = 0
-
-    for row_i, row in enumerate(grid):  # for each row
-        for col_i, col in enumerate(row):  # checking each unit of row
-
-            if col is 1:  # if index is land
-
-                # check above block
-                if row_i is 0:
-                    perimeter = perimeter + 1
-                else:
-                    if grid[row_i - 1][col_i] is not 1:
-                        perimeter = perimeter + 1
-
-                # check left block
-                if col_i is 0:
-                    perimeter = perimeter + 1
-                else:
-                    if grid[row_i][col_i - 1] is not 1:
-                        perimeter = perimeter + 1
-
-                # check right block
-                if col_i is (width - 1):
-                    perimeter = perimeter + 1
-                else:
-                    if grid[row_i][col_i + 1] is not 1:
-                        perimeter = perimeter + 1
-
-                # check below block
-                if row_i is (height - 1):
-                    perimeter = perimeter + 1
-                else:
-                    if grid[row_i + 1][col_i] is not 1:
-                        perimeter = perimeter + 1
-
-    return perimeter
+    for c in range(ht):
+        for h in range(wd):
+            if grid[c][h] == 0:
+                if (h > 0 and grid[c][h - 1] == 1):
+                    per += 1
+                if (c > 0 and grid[c - 1][h] == 1):
+                    per += 1
+                if (h < wd - 1 and grid[c][h + 1] == 1):
+                    per += 1
+                if (c < ht - 1 and grid[c + 1][h] == 1):
+                    per += 1
+    return per
